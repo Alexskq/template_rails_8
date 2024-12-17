401ae6c03a2617f93278be54424298b99399f8bf
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-12-17 18:52:08 +0100

    fix npm install 4

--- r8.rb
+++ r8.rb
@@ -213,13 +213,10 @@ after_bundle do
     run "npm i -D daisyui@latest"
   
     # Insérer 'require("daisyui")' dans tailwind.config.js
-    inject_into_file "config/tailwind.config.js", after: "plugins: [require('@tailwindcss/forms'),\n  require('@tailwindcss/typography'),\n  require('@tailwindcss/container-queries')," do
-      <<~RUBY
-        'require("daisyui"),'
-      RUBY
+    gsub_file "config/tailwind.config.js", /require\('@tailwindcss\/container-queries'\),/ do |match|
+      "#{match}\n    require(\"daisyui\"),"
     end
   end
-  
 
   # Heroku
   ########################################
